2a4b512e80b28de5ac958ac3993de7e6eabf0b20
thomas.kipar <thomas.kipar@localhost>
2011-12-11 17:39:42 +0100

    Added implementation-support using the "http://www.csu.org/emf4cpp" annotation.

---
+++ emf4cpp/emf4cpp/org.csu.emf4cpp.generator/src/org/csu/template/AnnotationHelper.java
@@ -0,0 +1,47 @@
+package org.csu.template;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.eclipse.emf.common.util.EMap;
+import org.eclipse.emf.ecore.EAnnotation;
+
+/**
+ * The Annotations helper contains mehtods to deals with {@link EAnnotation}.
+ * 
+ * <p>
+ * This is necessary since the current code generator uses the JavaBeans
+ * metamodel (that does not contian the "map" type for the annotations). It
+ * might be a better solution to change to the emf metamodel. As long this does
+ * not happen, one have to keep java code to deal with the annotations properly.
+ * </p>
+ * 
+ * @author Thomas Kipar
+ */
+public class AnnotationHelper {
+
+	/**
+	 * Finds all details in a given annotation.
+	 * 
+	 * @param pAnnotation
+	 *            the annotation to search details for.
+	 * @param pKey
+	 *            the key of the details to find.
+	 * @return all detail values that have the given key. Never
+	 *         <code>null</code> but possibly empty.
+	 */
+	public static List<String> findDetails(EAnnotation pAnnotation, String pKey) {
+		List<String> list = new ArrayList<String>();
+		if (pAnnotation != null && pKey != null) {
+			EMap<String, String> details = pAnnotation.getDetails();
+			for (Entry<String, String> entry : details.entrySet()) {
+				if (pKey.equals(entry.getKey())) {
+					list.add(entry.getValue());
+				}
+			}
+		}
+		return list;
+	}
+
+}
