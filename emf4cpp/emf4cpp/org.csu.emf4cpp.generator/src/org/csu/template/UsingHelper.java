ea59328580a520be40060a79b792be4658c3a6d7
thomas.kipar <thomas.kipar@localhost>
2012-05-24 14:31:32 +0200

    Fixed using declaratives: Now only the right declaratives are added.

--- emf4cpp/emf4cpp/org.csu.emf4cpp.generator/src/org/csu/template/UsingHelper.java
+++ emf4cpp/emf4cpp/org.csu.emf4cpp.generator/src/org/csu/template/UsingHelper.java
@@ -7,7 +7,7 @@ import java.util.Set;
 
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EOperation;
-
+import org.eclipse.emf.ecore.EPackage;
 
 /**
  * Utility class to create the using statements.
@@ -17,21 +17,63 @@ import org.eclipse.emf.ecore.EOperation;
 public class UsingHelper {
 
 	/**
-	 * For a given class, this method will return all operation names. Each name will
-	 * only be included once in the result, different signatures are not evaluated.
+	 * For a given class, this method will return the full qualified names of
+	 * all operations to use for the using declaratives.
 	 * 
-	 * @param pClass the class
+	 * @param pClass
+	 *            the class
 	 * @return all using declaratives
 	 */
-	public static List<String> findOperationNames(EClass pClass) {
+	public static List<String> findUsingOperations(EClass pClass) {
 		List<String> result = new ArrayList<String>();
 		Set<String> operations = new HashSet<String>();
+		Set<EOperation> addedOps = new HashSet<EOperation>();
+
 		if (pClass != null) {
 			for (EOperation eOp : pClass.getEAllOperations()) {
-				operations.add(eOp.getName());
-			}			
+				if (eOp.getEContainingClass() != pClass) {
+					boolean opWithSigAdded = false;
+					for (EOperation addedOp : addedOps) {
+						if (addedOp.getName().equals(eOp.getName())
+								&& addedOp.getEParameters().size() == eOp
+										.getEParameters().size()) {
+							boolean parametersMatch = true;
+							for (int i = 0; i < addedOp.getEParameters().size(); ++i) {
+								parametersMatch &= addedOp.getEParameters()
+										.get(i).getEType() == eOp
+										.getEParameters().get(i).getEType()
+										&& addedOp.getEParameters().get(i)
+												.isMany() == eOp
+												.getEParameters().get(i)
+												.isMany();
+							}
+							opWithSigAdded |= parametersMatch;
+						}
+					}
+
+					if (!opWithSigAdded) {
+						operations.add(toFQN(eOp));
+						addedOps.add(eOp);
+					}
+
+				}
+			}
 		}
 		result.addAll(operations);
 		return result;
+	}
+
+	private static String toFQN(EOperation pOp) {
+		StringBuilder builder = new StringBuilder();
+
+		EPackage p = pOp.getEContainingClass().getEPackage();
+		while (p != null) {
+			builder.insert(0, "::" + p.getName());
+			p = p.getESuperPackage();
+		}
+		builder.append("::").append(pOp.getEContainingClass().getName());
+		builder.append("::").append(pOp.getName());
+
+		return builder.toString();
 	}
 }
